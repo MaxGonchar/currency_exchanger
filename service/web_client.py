91159f228f198d792797ec0d98aa1a79cdbc876f
MaxGonchar <boborodada@gmail.com>
2021-10-14 16:44:08 +0300

    implement web client

--- service/web_client.py
+++ service/web_client.py
@@ -1,1 +1,24 @@
-"""Main web requests client"""
+import requests
+
+
+class WebClient:
+    """Main web requests client"""
+
+    def __init__(self, url):
+        self.url = url
+
+    def get_page(self):
+        response = self._request('get')
+        page = response.text
+        return page
+
+    def _request(self, method):
+        response = requests.request(method, self.url)
+        return response
+
+
+if __name__ == '__main__':
+    url = 'https://bank.gov.ua/ua/markets/exchangerates?date=14.10.2021&period=daily'
+    client = WebClient(url)
+    page = client.get_page()
+    print(page)
